b64e0aa8dad096d6ef0c02cd139307c26726f990
Glenn <glenn.adriaensens@thomasmore.be>
2023-11-23 16:58:48 +0100

    feat[get-details]: added fetch stock

--- src/controllers/parts/operations/get/get-details.ts
+++ src/controllers/parts/operations/get/get-details.ts
@@ -3,40 +3,53 @@ import { urlPart } from "../../resources";
 import { AxiosResponse } from "axios";
 import Map from "../../../../helpers/mapItems";
 import { APIPartParameter } from "../../../../models/APIPartParameter.model";
+import { APIPartStock } from "../../../../models/APIPartStock.model";
 
 const axios = require("axios");
 require("dotenv").config();
-
-const getDetailsUrl = (id: string, topic: string) => {
-	let url = urlPart;
-	switch (topic) {
-		case "Parameters":
-			url += `parameter/?part=${id}`;
-			break;
-
-		default:
-			throw new Error("No/wrong topic provided.");
-			break;
-	}
-	return url;
-};
 
 export const getDetails: Handler = (req, res, next) => {
 	if (!req.params.id) {
 		throw new Error("Invalid id");
 	}
-	const url = getDetailsUrl(req.params.id, req.params.detailTopic);
-	axios
-		.get(url, {
-			headers: {
-				Authorization: process.env.DB_TOKEN,
-			},
-		})
-		.then((response: AxiosResponse<APIPartParameter[]>) => {
-			return response.data;
-		})
-		.then((response: APIPartParameter[]) => {
-			res.header("Access-Control-Allow-Origin", "*");
-			res.json(response);
-		});
+	const id = req.params.id;
+	let url = process.env.DB_HOST;
+	switch (req.params.detailTopic) {
+		case "Parameters":
+			url += `/api/part/parameter/?part=${id}`;
+			axios
+				.get(url, {
+					headers: {
+						Authorization: process.env.DB_TOKEN,
+					},
+				})
+				.then((response: AxiosResponse<APIPartParameter[]>) => {
+					return response.data;
+				})
+				.then((response: APIPartParameter[]) => {
+					res.header("Access-Control-Allow-Origin", "*");
+					res.json(response);
+				});
+			break;
+		case "Stock":
+			url += `/api/stock/?part=${id}`;
+			axios
+				.get(url, {
+					headers: {
+						Authorization: process.env.DB_TOKEN,
+					},
+				})
+				.then((response: AxiosResponse<APIPartStock[]>) => {
+					return response.data;
+				})
+				.then((response: APIPartStock[]) => {
+					res.header("Access-Control-Allow-Origin", "*");
+					//TODO: Map this to correct format!
+					res.json(response);
+				});
+			break;
+
+		default:
+			throw new Error("No/wrong topic provided.");
+	}
 };
