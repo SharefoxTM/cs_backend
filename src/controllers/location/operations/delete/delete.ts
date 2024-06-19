9bff5bee844bc082ce8299705fe5698fbaa3c77b
Glenn <glenn.adriaensens@thomasmore.be>
2024-06-19 09:32:15 +0200

    feat: [untested] Delete location endpoint

--- src/controllers/location/operations/delete/delete.ts
+++ src/controllers/location/operations/delete/delete.ts
@@ -3,12 +3,16 @@ import { inventree } from "../../../../server";
 import { AxiosError, AxiosResponse } from "axios";
 
 export const deleteCategory: Handler = (req, res) => {
-    inventree
-        .delete(`api/stock/location/${req.params.id}/`)
-        .then((response: AxiosResponse) => {
-            res.status(204).json(response.data);
-        })
-        .catch((err: AxiosError) =>
-            res.status(err.response?.status || 400).json(err),
-        );
-}
+	if (req.headers.authorization === process.env.DELETE_TOKEN) {
+		inventree
+			.delete(`api/stock/location/${req.body.pk}/`)
+			.then((response: AxiosResponse) => {
+				res.status(204).json(response.data);
+			})
+			.catch((err: AxiosError) =>
+				res.status(err.response?.status || 400).json(err),
+			);
+		return;
+	}
+	res.status(403).json("Tokens do not match!");
+};
